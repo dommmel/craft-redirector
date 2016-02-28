e830519bca17b5e1b16f4076ef21923d03edfe1a
Dommmel <dommmel@gmail.com>
2016-02-28 12:06:38 +0100

    validate that legacy url is globally unique

--- redirector/fieldtypes/RedirectorFieldType.php
+++ redirector/fieldtypes/RedirectorFieldType.php
@@ -3,6 +3,7 @@ namespace Craft;
 
 class RedirectorFieldType extends BaseFieldType
 {
+
 	public function getName()
 	{
 		return Craft::t('Legacy URL');
@@ -22,24 +23,44 @@ class RedirectorFieldType extends BaseFieldType
 		));
 	}
 
+	/*
+	 * since we don't use a column in the craft_content table to store data
+	 * we use the "prepValueFromPost" to do the actual database changes
+	 */
 	public function prepValueFromPost($value)
 	{
 		$id = $this->element->id;
 		$locale = $this->element->locale;
 
-		// If value is empty delete database record. if not update the record.
+		// If value is empty delete the database record. if not update the record.
 		if (str_replace("/","", $value) == "") {
 			craft()->redirector->delete($id, $locale);
+			// If the value was set to "/" we can end up here even though no record exists. 
+			// Still no validation error should occur
+			$isValid = true;
 		} else {
 			// if first character is not a slash then tack it on
 			$value = '/' . ltrim($value, '/');
-			craft()->redirector->createOrUpdate($id, $locale, $value);
+			$isValid = craft()->redirector->createOrUpdate($id, $locale, $value);
 		}
+		// Skip the validation function (by returning false) if db operation was successfull
+		return $isValid ? false : $value;
 	}
 
 	public function defineContentAttribute()
 	{
 		return false;
+	}
+
+	/*
+	* The validate function seem to only get called if "prepValueFromPost"
+	* above returns a truthy value. So we let the validation happen there
+	* based on the db constrains and use the "validate" function purely to
+	*  display the error messages.
+	*/
+	public function validate($value)
+	{
+		return '"'.$value.'"' . Craft::t(' is already being used in another entry.');
 	}
 
 }
